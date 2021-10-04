d423cf475eeabe71aba81d740e7e5276101eef3c
jzabala1 <57100445+jzabala1@users.noreply.github.com>
2021-10-04 11:22:51 -0400

    finished lab

--- Lab 5/main.js
+++ Lab 5/main.js
@@ -1,9 +1,29 @@
 function addNewList() {
-    alert('hello world!');
+    alert('hello world alert!');
 }
 
 function addListItem() {
-    console.log("hello world!")
+    let list = document.getElementById("grocery-list");
+    let itemInput = document.getElementById("new-list-item");
+    let newItem = document.createElement("li");
+    newItem.appendChild(document.createTextNode(itemInput.value));
+    list.appendChild(newItem);
+    console.log("hello world console!")
+}
+
+function deleteListItem(item) {
+        // remove li element (item) from ol element (item.parentNode)
+        item.parentNode.removeChild(item);
+}
+
+function completeListItem(item) {
+    if (item.checked) { // true if the input checkbox is checked
+        item.parentNode.style.textDecoration = "line-through";
+        // in css, this would be: "text-decoration: line-through"
+    } else {
+        item.parentNode.style.textDecoration = "none";
+        // in css, this would be: "text-decoration: none"
+    }
 }
 
 
